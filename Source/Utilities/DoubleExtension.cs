7151c6c9d60816f42b45c1e7bb7ac3c2d5bf9bda
Andrea Mattei <37371332+andreamattei97@users.noreply.github.com>
2019-02-17 14:35:39 +0100

    Added Double IsFinite extension

---
+++ Source/Utilities/DoubleExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Beryl.Utilities
+{
+    public static class DoubleExtension
+    {
+        //extension method for cheking if a number is not infinity or NaN
+        public static bool IsFinite(this Double number)
+        {
+            return !Double.IsInfinity(number) && !Double.IsNaN(number);
+        }
+    }
+}
