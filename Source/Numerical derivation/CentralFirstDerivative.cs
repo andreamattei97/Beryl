b012136c8f8c254d0249a848408cda05cc9a2bc1
Andrea Mattei <andreamattei97@yahoo.com>
2019-02-19 13:15:11 +0100

    Numerical derivation added

---
+++ Source/Numerical derivation/CentralFirstDerivative.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Beryl.Utilities;
+
+namespace Beryl.NumericalDerivation
+{
+    //first order numerical derivative with central finite difference
+    class CentralFirstDerivative :NumericalDerivative
+    {
+
+        public CentralFirstDerivative(Func<double, double> function, double h) : base(function, h) { }
+
+        public override int Order => 1;
+
+        public override double CalculateDerivative(double x)
+        {
+            double result = 0;
+
+            result = (Function(x + H / 2) - Function(x - H / 2)) / H;
+
+            if (!result.IsFinite())
+            {
+                throw new CalculationException("Invalid derivated evaluated", passedFunction: Function);
+            }
+
+            return result;
+        }
+
+        public override double CalculateFiniteDifference(double x)
+        {
+            double result = 0;
+
+            result = (Function(x + H / 2) - Function(x - H / 2));
+
+            if (!result.IsFinite())
+            {
+                throw new CalculationException("Invalid function evaluated", passedFunction: Function);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Source/Numerical derivation/NumericalDerivative.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Beryl.Utilities;
+
+namespace Beryl.NumericalDerivation
+{
+    abstract class NumericalDerivative
+    {
+        //the spacing interval
+        private double _h;
+        public double H
+        {
+            get
+            {
+                return _h;
+            }
+
+            set
+            {
+                if (value.IsFinite() || value <= 0 )
+                    throw new ArgumentOutOfRangeException("not valid spacing h (h must be a finite positive number");
+                _h = value;
+            }
+        }
+
+        //the function of which the derivative is approximated
+        public Func<double, double> Function
+        {
+            get;
+            private set;
+        }
+
+        //gets the order of the derivative
+        public abstract int Order { get; }
+
+        public NumericalDerivative(Func<double, double> function,double h)
+        {
+            H = h;
+            Function = function;
+        }
+
+        //calculates the finite difference of the function on the spacing intervale H
+        public abstract double CalculateFiniteDifference(double x);
+        //calculates the numerical approximated derivative using the spacing H
+        public abstract double CalculateDerivative(double x);
+
+    }
+}
