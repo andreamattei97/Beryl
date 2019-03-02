4baee1917bcec98f52b25adb5bfe057a307d9f13
Andrea Mattei <37371332+andreamattei97@users.noreply.github.com>
2019-03-03 00:53:25 +0100

    Update SuccessiveRelativeCriteria.cs

--- Source/RootFinding/SuccessiveRelativeCriteria.cs
+++ Source/RootFinding/SuccessiveRelativeCriteria.cs
@@ -15,7 +15,7 @@ namespace Beryl.RootFinding
             }
             private set
             {
-                if (_tolerance > 0)
+                if (value < 0)
                     throw new ArgumentOutOfRangeException("Tolerance", "The tolerance must be non-negative");
                 _tolerance = value;
             }
