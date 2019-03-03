37e22b90a96576099fd7be71c912da437f6287a0
Andrea Mattei <arabum97@live.com>
2019-03-04 00:35:06 +0100

    Completed Newton solver and added secant solver

--- Source/RootFinding/NewtonSolver.cs
+++ Source/RootFinding/NewtonSolver.cs
@@ -1,12 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Beryl.NumericalDerivation;
+using Beryl.Structures;
+using Beryl.Utilities;
 
-namespace CalcoloNumerico.Beryl.RootFinding
+namespace Beryl.RootFinding
 {
     class NewtonSolver
     {
+        private int _maxIterations = 50;
+        public int MaxIterations
+        {
+            get { return _maxIterations; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("MaxIterations", "The number of iteractions must be non-negative");
+                _maxIterations = value;
+            }
+        }
+
+        //the stopping criteria
+        public IStoppingCriteria StoppingCriteria
+        {
+            get;
+        }
+
+        public Func<double, double> Function { get; }
+        //the function wrapped for detecting non-finite returned values
+        private readonly Func<double, double> _wrappedFunction;
+
+        //the function derivative
+        public Func<double,double> Derivative { get; }
+        //the derivative wrapped for detecting non-finite returned values
+        private readonly Func<double, double> _wrappedDerivative;
+
+        public NewtonSolver(Func<double,double> function,Func<double,double> derivative,IStoppingCriteria stoppingCriteria)
+        {
+            Function = function;
+            _wrappedFunction = FunctionWrapper.MakeWrapper(function);
+
+            Derivative = derivative;
+            _wrappedDerivative = FunctionWrapper.MakeWrapper(derivative);
+
+            StoppingCriteria = stoppingCriteria;
+        }
+        
+        public NewtonSolver(Func<double,double> function,IStoppingCriteria stoppingCriteria):this(function,CentralDerivative.MakeDerivative(function,1,0.0001),stoppingCriteria)
+        { }
+
+        public NewtonSolver(Func<double,double> function):this(function,new AbsoluteCriteria(0.000001))
+        { }
+
+        public NewtonSolver(Func<double, double> function, Func<double, double> derivative) : this(function, derivative, new AbsoluteCriteria(0.000001))
+        { }
+
+        public Result Solve(double initialEstimation,int multiplicity=1)
+        {
+            if (multiplicity < 1)
+                throw new ArgumentOutOfRangeException("multiplicity", "the multiplicity of the root must be equal or greater than 1");
+
+            double xn = initialEstimation;
+            double fn = 0;
+            double dfn = 0;
+            bool success = false;
+
+            StoppingCriteria.SetCriteria(new Vector2D(xn, _wrappedFunction(xn)));
+
+            for(int k=0;k<MaxIterations;k++)
+            {
+                fn = _wrappedFunction(xn);
+                dfn = _wrappedDerivative(xn);
+
+                if (dfn == 0 && fn != 0)
+                    throw new CalculationException("Cannot continue calculation, the function derivative is 0 in " + xn, Function);
+
+                if(StoppingCriteria.FullfilCriteria(new Vector2D(xn,fn)))
+                {
+                    success = true;
+                    break;
+                }
+
+                xn = xn -multiplicity * fn / dfn;
+            }
+
+            return new Result(new Vector2D(xn,fn),success);
+        }
+        
+
     }
 }
---
+++ Source/RootFinding/SecantSolver.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Beryl.Structures;
+using Beryl.Utilities;
+
+namespace Beryl.RootFinding
+{
+    class SecantSolver
+    {
+        private int _maxIterations = 50;
+        public int MaxIterations
+        {
+            get { return _maxIterations; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("MaxIterations", "The number of iteractions must be non-negative");
+                _maxIterations = value;
+            }
+        }
+
+        //the stopping criteria
+        public IStoppingCriteria StoppingCriteria
+        {
+            get;
+        }
+
+        public Func<double, double> Function { get; }
+        //the function wrapped for detecting non-finite returned values
+        private readonly Func<double, double> _wrappedFunction;
+
+        public SecantSolver(Func<double, double> function, IStoppingCriteria stoppingCriteria)
+        {
+            Function = function;
+            _wrappedFunction = FunctionWrapper.MakeWrapper(function);
+
+            StoppingCriteria = stoppingCriteria;
+        }
+
+        //set an absolute tollerance of 10^-6 as stopping criteria
+        public SecantSolver(Func<double, double> function) : this(function, new AbsoluteCriteria(0.000001))
+        { }
+
+        public Result Solve(double x0, double x1)
+        {
+
+            double xn1 = x1,xn0=x0,x=0;
+            double fn1 = _wrappedFunction(xn1),fn0=_wrappedFunction(xn0);
+            bool success = false;
+
+            StoppingCriteria.SetCriteria(new Vector2D(xn1, _wrappedFunction(fn1)));
+
+            for (int k = 0; k < MaxIterations; k++)
+            {
+                double d = (fn1 - fn0)/ (xn1 - xn0);
+                x = xn1 - fn1/ d ;
+                if (!x.IsFinite())
+                    throw new CalculationException("Found a non finite point in the succession to the root", Function);
+
+                fn0 = fn1;
+                xn0 = xn1;
+                fn1 = _wrappedFunction(x);
+                xn1 = x;
+
+                if (StoppingCriteria.FullfilCriteria(new Vector2D(xn1, fn1)))
+                {
+                    success = true;
+                    break;
+                }
+            }
+
+            return new Result(new Vector2D(xn1, fn1), success);
+        }
+    }
+}
