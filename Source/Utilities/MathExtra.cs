c94c2c160dce283a6181bf6ba137cbc440589a30
andreamattei97 <arabum97@live.com>
2019-04-02 01:30:41 +0200

    Added a function for finding the nearest greater power of 2 of a number

--- Source/Utilities/MathExtra.cs
+++ Source/Utilities/MathExtra.cs
@@ -12,18 +12,18 @@ namespace Beryl.Utilities
                 throw new ArgumentOutOfRangeException("number", "The number must be non-negative");
 
             int result = 1;
-            for(int i=1;i<=number;i++)
+            for (int i = 1; i <= number; i++)
             {
                 result *= i;
             }
 
             return result;
         }
-        
+
         //calculates the binomial coefficient (n k)
-        public static int BinomialCoefficient(int n,int k)
+        public static int BinomialCoefficient(int n, int k)
         {
-            if(n < 0)
+            if (n < 0)
                 throw new ArgumentOutOfRangeException("n", "n must be non-negative");
             if (k > n)
                 throw new ArgumentOutOfRangeException("k", "k must be less than n");
@@ -31,5 +31,26 @@ namespace Beryl.Utilities
                 throw new ArgumentOutOfRangeException("k", "k must be non-negative");
             return Factorial(n) / (Factorial(k) * Factorial(n - k));
         }
+
+        //calculates the nearest greater power of 2 of a number (for negatives numbers it finds the nearest power of 2 of its module)
+        public static int Superior2Power(int n)
+        {
+            int result = 1;
+
+            if (n < 0) //calculates the module
+                n = -n;
+            else if (n == 0) //no operations required
+                return result;
+
+            n -= 1; //for adjusting the result if n is a power of 2
+
+            while (n != 0)
+            {
+                n = n >> 1;
+                result *= 2;
+            }
+            return result;
+        }
+
     }
 }
