6723ceda995df0f77b8f57cc45978e02a463e453
andreamattei97 <arabum97@live.com>
2019-04-03 00:58:39 +0200

    Added IDiscretizer interface and UniformDiscretizer class

---
+++ Source/ODE/IDiscretizer.cs
@@ -0,0 +1,15 @@
+﻿using Beryl.Structures;
+
+namespace Beryl.ODE
+{
+    //the interface for ODE discretizer
+    interface IDiscretizer
+    {
+        //returns the step for moving to the left of the given point
+        double CalculateLeftStep(Vector2D currentPoint);
+        //returns the step for moving to the right of the given point
+        double CalculateRightStep(Vector2D currentPoint);
+        //find the interpolated step of x, a point in the interval [previousPoint,nextPoint]
+        double IntermediateStep(double x, Vector2D previousPoint, Vector2D nextPoint);
+    }
+}
---
+++ Source/ODE/UniformDiscretizer.cs
@@ -0,0 +1,32 @@
+﻿using Beryl.Structures;
+
+namespace Beryl.ODE
+{
+    //used for having uniform discretization (constant step)
+    class UniformDiscretizer : IDiscretizer
+    {
+        //the constant modulus of the step
+        public double StepModulus { get; }
+
+        //sets the modulus of the step
+        public UniformDiscretizer(double stepModulus) => StepModulus = stepModulus;
+
+        //negative step for moving to the left
+        public double CalculateLeftStep(Vector2D currentPoint)
+        {
+            return -StepModulus;
+        }
+
+        //positive step for moving to the right
+        public double CalculateRightStep(Vector2D currentPoint)
+        {
+            return StepModulus;
+        }
+
+        //calculates the interpolated step using linear interpolation
+        public double IntermediateStep(double x, Vector2D previousPoint, Vector2D nextPoint)
+        {
+            return x - previousPoint.x;
+        }
+    }
+}
