eb31911c73066ca221cd8d3a7af612ef7b1c6228
andreamattei97 <arabum97@live.com>
2019-08-11 01:09:25 +0200

    missing commit

--- Source/NumericalIntegration/DefaultIntegrationParameters.cs
+++ Source/NumericalIntegration/DefaultIntegrationParameters.cs
@@ -1,13 +1,24 @@
-﻿namespace Beryl.NumericalIntegration
+﻿using System;
+
+namespace Beryl.NumericalIntegration
 {
     public static class DefaultIntegrationParameters
     {
         public static readonly double INITIAL_DEFAULT_MAX_STEP = 0.01;
 
+        private static double _defaultMaxStep;
         public static double DefaultMaxStep
         {
-            get;
-            set;
+            get
+            {
+                return _defaultMaxStep;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("value", "The maximum step must be positive");
+                _defaultMaxStep = value;
+            }
         }
 
         static DefaultIntegrationParameters()
